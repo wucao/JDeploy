98818abdac9500bbe8ab069a5492d21bb3abbee7
wucao <806223819@qq.com>
2015-08-25 10:18:45 +0800

    增加表

--- doc/sql.sql
+++ doc/sql.sql
@@ -5,3 +5,12 @@ CREATE TABLE `java_deploy` (
   `url` varchar(255) NOT NULL COMMENT 'svn/git地址',
   PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `java_web_deploy` (
+  `uuid` varchar(36) NOT NULL COMMENT 'UUID',
+  `name` varchar(255) NOT NULL COMMENT '项目名称',
+  `final_name` varchar(255) NOT NULL COMMENT 'final_name',
+  `url` varchar(255) NOT NULL COMMENT 'svn/git地址',
+  `context_path` varchar(255) NOT NULL COMMENT 'Web项目contextPath',
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
